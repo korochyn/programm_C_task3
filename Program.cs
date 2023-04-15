c083d2417c0e7b5c153481d2381405228be269c3
Dmitiy Kochkin <dmm_dmm_kdv@example.com>
2023-04-15 13:01:44 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int result_1 = number / 2;
+int result_2 = number - result_1*2;
+// Console.WriteLine(result_2);
+if (result_2 == 0) {Console.WriteLine("Число четное");}
+else {Console.WriteLine("Число  не четное");}
